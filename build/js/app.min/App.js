40953e8f3fe4f7d72db05fcbbc16dfd87b5b0368
Francesco Zaia <francesco.zaia@gmail.com>
2014-03-30 20:36:37 +0100

    removed build folders

--- build/js/app.min/App.js
+++
@@ -1,5 +0,0 @@
-/*
- * A generic template for self-development. v0.1.0
- * (c) 2014/03/30 
- */
-!function(a){"use strict";function b(){console.log(c)}var c="Yo!";a.message="Yo two!",a.initialize=function(){b()}}(window.app=window.app||{}),app.initialize();
